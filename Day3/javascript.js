2c6bc7ed67bed62c217449322936d3dd60390d90
mohammedabrar8341-blip <mohammedabrar8341@gmail.com>
2026-06-14 15:21:29 +0530

    Day3

---
+++ Day3/javascript.js
@@ -0,0 +1,175 @@
+// PROBLEM-1
+// let num=-5;
+// if(num>0){
+//     console.log("positive number")
+// }
+// else if (num<0){
+//     console.log("negative number")
+// }
+// PROBLEM-2
+// let age=17;
+// if (age>=18){
+//     console.log("You are eligible for license")
+// }
+// else (age<18);{
+//     console.log("Not eligile for license")
+// }
+// PROBLEM-3
+// let userMarks=Number(prompt("Enter marks"));
+// if(userMarks>=90){
+//     console.log("A")
+// }else if (userMarks>=80 && userMarks<=89){
+//     console.log("B")
+// }else if (userMarks>=70 && userMarks<=79){
+//     console.log("C")
+// }else if (userMarks>=60 && userMarks<=69){
+//     console.log("D")
+// }else if (userMarks<60) {
+//     console.log("F")
+// }
+// PROBLEM-4
+// let userName=prompt("Enter user name");
+// let passWord=prompt("Enter your password");
+// if (userName=="admin" && passWord==1234){
+//     console.log("Login successful")
+// }else {
+//     console.log("Login unsuccessful")
+// }
+// PROBLEM-5
+// let day="Monday";
+// if (day=="Saturday"||day=="Sunday"){
+//     console.log("Weekend")
+// }else {
+//     console.log("Weekday")
+// }
+// PROBLEM-6
+// let age=25;
+// if (age<12){
+//     console.log("Ticket price:$5")
+// }else if (age>=12 && age<60){
+//     console.log("Ticket price:$8")
+// }else if (age>=60) {
+//     console.log("Ticket price:$6")
+// }
+//  PROBLEM-7
+// let orderTotal=40;
+// let isPremium="true";
+// if (orderTotal>50){
+//     console.log("Free shipping")
+// }else if (isPremium=="true"){
+//     console.log('Free shipping')
+// }
+// else {
+//     console.log("Thank")
+// }
+// PROBLEM-8
+// let marks=75;
+// if (marks>=33){
+//     if(marks>=80){
+//         console.log("Pass with destication")
+//     }else{
+//         console.log("Pass")
+//     }
+
+// }else{
+//     console.log("fail")
+// }
+
+// PROBLEM-9
+// let temperature = 25;
+// let isRaining = false;
+// if (temperature> 30) {
+//     console.log("Hot day");
+// } else if (temperature>= 20 && temperature<= 30) {
+//     if (isRaining) {
+//         console.log("Pleasant but rainy");
+//     } else {
+//         console.log("Perfect weather");
+//     }
+// } else {
+//     console.log("Cold day");
+// }
+
+// PROBLEM-10
+// let dayNum =3;
+// if (dayNum==1){
+//     console.log("Monday")
+// }else if(dayNum==2){
+//     console.log("Tuesday")
+// }else if (dayNum==3){
+//     console.log("Wednesday")
+// }else if (dayNum==4){
+//     console.log("Wednesday")
+// }else if (dayNum==5){
+//     console.log("Thusday")
+// }else if (dayNum==6){
+//     console.log("Friday")
+// }else if (dayNum==7){
+//     console.log("Sunday")
+// }
+// PROBLEM-11
+// let signal="yellow";
+// if (signal=="red"){
+//     console.log("Stop")
+// }else if (signal=="yellow"){
+//     console.log("Slow down")
+// }else if (signal=="green"){
+//     console.log("Go")
+// }else {
+//     console.log("Invited signal")
+// }
+
+// PROBLEM-12
+// let month=7;
+// switch(month){
+//     case 12:
+//         case 1:
+//             case 2:
+// console.log("Winter");
+//    break;
+//    case 3:
+//     case 5:
+//         case 4:
+//    console.log("Spring");
+// break;
+// case 6:
+//     case 7:
+//         case 8:
+//     console.log("Summer");
+//     break
+//     case 9:
+//         case 10:
+//             case 11:
+//         console.log("Fall");
+//         break;
+//         default :
+//         console.log("Not found");
+// }
+
+// PROBLEM-13
+// let userName="";
+// if (userName){
+//     console.log("Welcome"+userName)
+// }else{
+//     console.log("Please enter userName")
+// }
+
+// PROBLEM-14
+// let score=0;
+// if (score){
+//     console.log("Yes")
+// }else{
+//     console.log("No score yet")
+// }
+
+// PROBLEM-15
+// let age=20;
+// let isCitizen="true";
+// let hasVoted="false";
+// if (age>=18 && isCitizen && hasVoted){
+//        console.log("Can vote")
+// }else {
+//     console.log("Canot vote")
+// }
+
+
